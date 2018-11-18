7e1921c9e85b85542b1ad0736f98935e8481f675
Ian Moon <imm3350@g.rit.edu>
2018-11-18 15:59:20 -0500

    Added untracked files

---
+++ client/creator/creator.js
@@ -0,0 +1,178 @@
+let cardsList = {};
+let cardCount = 0;
+
+let timeout = null;
+
+const addCard = (card) => {
+    cardsList[cardCount] = card;
+    cardCount++;
+    
+    ReactDOM.render(
+        <DeckList cards={[cardsList]} />, document.querySelector("#deckList")
+    );
+};
+
+const removeCard = (e) => {
+    delete cardsList[e];
+    cardCount--;
+    ReactDOM.render(
+        <DeckList cards={[cardsList]} />, document.querySelector("#deckList")
+    );
+};
+
+const handleDeck = (e) => {
+    e.preventDefault();
+    
+    const name = $("#deckName").val();
+    
+    if(name.trim() == '' || Object.keys(cardsList).length === 0) {
+        return false;
+    }
+    
+    let deck = {
+        deckName: name,
+        cards: cardsList
+    };
+    
+    console.log($("#deckForm").serialize() + "&cards=" + JSON.stringify(cardsList));
+    sendAjax('POST', $("#deckForm").attr("action"), $("#deckForm").serialize() + "&cards=" + JSON.stringify(cardsList), function() {
+    }, false);
+    
+    return false;
+};
+
+const handleSearch = (e) => {
+    e.preventDefault();
+    
+    if($("#cardName").val().trim() === '') {
+        return false;
+    }
+    
+    //Clearing the timeout
+    clearTimeout(timeout);
+    
+    //Only querying the server every 500 ms
+    timeout = setTimeout(() => {
+        return loadCardsFromServer($("#cardName").val());
+    }, 500);
+};
+
+const DeckForm = (props) => {
+    return (
+        <form id="deckForm" onSubmit={handleDeck} name="deckForm" action="/create" method="POST" className="deckForm">
+            <input className="form-control" id="deckName" type="text" name="name" placeholder="Enter a deck name..." required/>
+            <input type="hidden" name="_csrf" value={props.csrf} />
+            <input className="formSubmit" type="submit" value="Submit Deck" />
+        </form>
+    );
+};
+const SearchForm = (props) => {
+    return (
+        <form id="searchForm" onSubmit={handleSearch} name="searchForm" action="/getCards" method="GET" className="searchForm">
+            <input className="form-control" id="cardName" type="text" onChange={handleSearch} name="name" placeholder="Type to search for a card..." required />
+        </form>
+    );
+};
+
+const CardList = function(props) {
+    if(props.cards.length === 0) {
+        return (
+            <div className="container-fluid pt-3">
+                <div className="cardList row flex-row" align="center">
+                    <h3 className="emptyCard">No cards match search</h3>
+                </div>
+            </div>
+        );
+    }
+    
+    const cardNodes = props.cards[0].map((card) => {
+        return (
+            <div key={card.id} className="card col-xs-2" align="center">
+                <img className="card-img-top" src={card.imageUrl} alt={card.name} onClick={() => addCard(card)}/>
+            </div>
+        );
+    });
+    
+    return (
+        <div className="container-fluid pt-3">
+            <div className="cardList row flex-row" align="center">
+                {cardNodes}
+            </div>
+        </div>
+    );
+};
+
+const DeckList = function(cardList) {
+    if(Object.keys(cardsList).length === 0) {
+        return (
+            <div className="container-fluid pt-3 rounded">
+                <div className="cardList row flex-row" align="center">
+                    <h3 className="emptyCard">No cards in Deck</h3>
+                </div>
+            </div>
+        );
+    }
+    
+    const cardNodes = [];
+    const keys = Object.keys(cardsList);
+    
+    for(let i = 0; i < cardCount; i++) {
+        cardNodes[i] = (<div key={i} className="card col-xs-2" align="center">
+                <img className="card-img-top" src={cardsList[keys[i]].imageUrl} alt={cardsList[keys[i]].name} onClick={(e) => removeCard(keys[i])}/>
+            </div>)
+    }
+    
+    return (
+        <div className="container-fluid pt-3 rounded">
+            <div className="cardList row flex-row" align="center">
+                {cardNodes}
+            </div>
+        </div>
+    );
+};
+
+const loadCardsFromServer = (cardName) => {
+    sendAjax('GET', '/getCards', {name: cardName}, (data) => {
+        //Making sure we only display cards with an image
+        let cards = [];
+        console.log(data.cards);
+        for(let i = 0; i < data.cards.length; i++) {
+            if(data.cards[i].imageUrl) {
+                cards.push(data.cards[i]);
+            }
+        }
+        console.log(cards);
+        ReactDOM.render(
+            <CardList cards={[cards]} />, document.querySelector("#searchResults")
+        );
+    });
+};
+
+const setup = function(csrf) {
+    ReactDOM.render(
+        <DeckForm csrf={csrf} />, document.querySelector("#deck")
+    );
+    ReactDOM.render(
+        <SearchForm csrf={csrf} />, document.querySelector("#search")
+    );
+    
+    ReactDOM.render(
+        <CardList cards={[]} />, document.querySelector("#searchResults")
+    );
+    
+    ReactDOM.render(
+        <DeckList cards={[]} />, document.querySelector("#deckList")
+    );
+    
+    loadCardsFromServer("");
+};
+
+const getToken = () => {
+    sendAjax('GET', '/getToken', null, (result) => {
+        setup(result.csrfToken);
+    });
+};
+
+$(document).ready(function() {
+    getToken();
+});
---
+++ hosted/creatorBundle.js
@@ -0,0 +1,222 @@
+"use strict";
+
+var cardsList = {};
+var cardCount = 0;
+
+var timeout = null;
+
+var addCard = function addCard(card) {
+    cardsList[cardCount] = card;
+    cardCount++;
+
+    ReactDOM.render(React.createElement(DeckList, { cards: [cardsList] }), document.querySelector("#deckList"));
+};
+
+var removeCard = function removeCard(e) {
+    delete cardsList[e];
+    cardCount--;
+    ReactDOM.render(React.createElement(DeckList, { cards: [cardsList] }), document.querySelector("#deckList"));
+};
+
+var handleDeck = function handleDeck(e) {
+    e.preventDefault();
+
+    var name = $("#deckName").val();
+
+    if (name.trim() == '' || Object.keys(cardsList).length === 0) {
+        return false;
+    }
+
+    var deck = {
+        deckName: name,
+        cards: cardsList
+    };
+
+    console.log($("#deckForm").serialize() + "&cards=" + JSON.stringify(cardsList));
+    sendAjax('POST', $("#deckForm").attr("action"), $("#deckForm").serialize() + "&cards=" + JSON.stringify(cardsList), function () {}, false);
+
+    return false;
+};
+
+var handleSearch = function handleSearch(e) {
+    e.preventDefault();
+
+    if ($("#cardName").val().trim() === '') {
+        return false;
+    }
+
+    //Clearing the timeout
+    clearTimeout(timeout);
+
+    //Only querying the server every 500 ms
+    timeout = setTimeout(function () {
+        return loadCardsFromServer($("#cardName").val());
+    }, 500);
+};
+
+var DeckForm = function DeckForm(props) {
+    return React.createElement(
+        "form",
+        { id: "deckForm", onSubmit: handleDeck, name: "deckForm", action: "/create", method: "POST", className: "deckForm" },
+        React.createElement("input", { className: "form-control", id: "deckName", type: "text", name: "name", placeholder: "Enter a deck name...", required: true }),
+        React.createElement("input", { type: "hidden", name: "_csrf", value: props.csrf }),
+        React.createElement("input", { className: "formSubmit", type: "submit", value: "Submit Deck" })
+    );
+};
+var SearchForm = function SearchForm(props) {
+    return React.createElement(
+        "form",
+        { id: "searchForm", onSubmit: handleSearch, name: "searchForm", action: "/getCards", method: "GET", className: "searchForm" },
+        React.createElement("input", { className: "form-control", id: "cardName", type: "text", onChange: handleSearch, name: "name", placeholder: "Type to search for a card...", required: true })
+    );
+};
+
+var CardList = function CardList(props) {
+    if (props.cards.length === 0) {
+        return React.createElement(
+            "div",
+            { className: "container-fluid pt-3" },
+            React.createElement(
+                "div",
+                { className: "cardList row flex-row", align: "center" },
+                React.createElement(
+                    "h3",
+                    { className: "emptyCard" },
+                    "No cards match search"
+                )
+            )
+        );
+    }
+
+    var cardNodes = props.cards[0].map(function (card) {
+        return React.createElement(
+            "div",
+            { key: card.id, className: "card col-xs-2", align: "center" },
+            React.createElement("img", { className: "card-img-top", src: card.imageUrl, alt: card.name, onClick: function onClick() {
+                    return addCard(card);
+                } })
+        );
+    });
+
+    return React.createElement(
+        "div",
+        { className: "container-fluid pt-3" },
+        React.createElement(
+            "div",
+            { className: "cardList row flex-row", align: "center" },
+            cardNodes
+        )
+    );
+};
+
+var DeckList = function DeckList(cardList) {
+    if (Object.keys(cardsList).length === 0) {
+        return React.createElement(
+            "div",
+            { className: "container-fluid pt-3 rounded" },
+            React.createElement(
+                "div",
+                { className: "cardList row flex-row", align: "center" },
+                React.createElement(
+                    "h3",
+                    { className: "emptyCard" },
+                    "No cards in Deck"
+                )
+            )
+        );
+    }
+
+    var cardNodes = [];
+    var keys = Object.keys(cardsList);
+
+    var _loop = function _loop(i) {
+        cardNodes[i] = React.createElement(
+            "div",
+            { key: i, className: "card col-xs-2", align: "center" },
+            React.createElement("img", { className: "card-img-top", src: cardsList[keys[i]].imageUrl, alt: cardsList[keys[i]].name, onClick: function onClick(e) {
+                    return removeCard(keys[i]);
+                } })
+        );
+    };
+
+    for (var i = 0; i < cardCount; i++) {
+        _loop(i);
+    }
+
+    return React.createElement(
+        "div",
+        { className: "container-fluid pt-3 rounded" },
+        React.createElement(
+            "div",
+            { className: "cardList row flex-row", align: "center" },
+            cardNodes
+        )
+    );
+};
+
+var loadCardsFromServer = function loadCardsFromServer(cardName) {
+    sendAjax('GET', '/getCards', { name: cardName }, function (data) {
+        //Making sure we only display cards with an image
+        var cards = [];
+        console.log(data.cards);
+        for (var i = 0; i < data.cards.length; i++) {
+            if (data.cards[i].imageUrl) {
+                cards.push(data.cards[i]);
+            }
+        }
+        console.log(cards);
+        ReactDOM.render(React.createElement(CardList, { cards: [cards] }), document.querySelector("#searchResults"));
+    });
+};
+
+var setup = function setup(csrf) {
+    ReactDOM.render(React.createElement(DeckForm, { csrf: csrf }), document.querySelector("#deck"));
+    ReactDOM.render(React.createElement(SearchForm, { csrf: csrf }), document.querySelector("#search"));
+
+    ReactDOM.render(React.createElement(CardList, { cards: [] }), document.querySelector("#searchResults"));
+
+    ReactDOM.render(React.createElement(DeckList, { cards: [] }), document.querySelector("#deckList"));
+
+    loadCardsFromServer("");
+};
+
+var getToken = function getToken() {
+    sendAjax('GET', '/getToken', null, function (result) {
+        setup(result.csrfToken);
+    });
+};
+
+$(document).ready(function () {
+    getToken();
+});
+"use strict";
+
+var handleError = function handleError(message) {
+    $("#errorMessage").text(message);
+    $("#domoMessage").animate({ width: 'toggle' }, 350);
+};
+
+var redirect = function redirect(response) {
+    $("#domoMessage").animate({ width: 'hide' }, 350);
+    window.location = response.redirect;
+};
+
+var sendAjax = function sendAjax(type, action, data, success, process) {
+    var processInfo = true;
+    if (process) {
+        processInfo = process;
+    }
+    $.ajax({
+        cache: false,
+        type: type,
+        url: action,
+        data: data,
+        dataType: "json",
+        processData: processInfo,
+        success: success,
+        error: function error(xhr, status, _error) {
+            var messageObj = JSON.parse(xhr.responseText);
+            handleError(messageObj.error);
+        }
+    });
+};
